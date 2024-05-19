95d05f1a26d5085c468e3132737a0ee26b7d6818
kokk <kokk7310@gmail.com>
2024-05-19 21:13:19 +0900

    #19 - 게시글 및 댓글 조회 api 추가
    게시글 및 댓글 조회 api 추가

---
+++ src/main/java/com/example/projectboard/dto/ArticleDto.java
@@ -0,0 +1,15 @@
+package com.example.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        LocalDateTime createAt,
+        String createdBy,
+        String title,
+        String content,
+        String hashtag
+) {
+  public static ArticleDto of(LocalDateTime createAt, String createdBy, String title, String content, String hashtag) {
+    return new ArticleDto(createAt, createdBy, title, content, hashtag);
+  }
+}
